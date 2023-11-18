4e1e4ba0770cef7cf2efc7b443dd8e35297c04e5
Angel Tapia <150973344+AxorXT@users.noreply.github.com>
2023-11-18 14:35:26 -0600

    Creación de calculadora
    Mi primer trabajo en la carrera de programación

--- Parcial 1/Calculadora.cpp
+++ Parcial 1/Calculadora.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main(){
+
     int num1, num2, resultado;
+
     printf("Escriba la primera cantidad: \n");
     scanf("%d", &num1);
     printf("Escriba la segunda cantidad: \n");
