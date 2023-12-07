efea342ef67c90de427c5fd605702e0e63c0f164
Angel <angel.sanchez@uniat.edu.mx>
2023-12-06 22:57:55 -0600

    Examen Parcial 3

--- ExamenP3/poliza.h
+++ ExamenP3/poliza.h
@@ -6,13 +6,9 @@
 #include <random>
 #include <algorithm>
 
-    //Creamos clase Poliza
+//Creamos clase Poliza
 class Poliza {
 public:
-    std::string nombre, curp, nacimiento;
-    int edad, year, prestamo, deducible;
-
-    Poliza(){}
 
     Poliza (std::string nombre, std::string curp, std::string nacimiento, int year, int edad, int prestamo, int deducible){
         this -> nombre = nombre;
@@ -34,7 +30,35 @@ public:
         return random_number; //Retornamos el resultado para usarlo a nuestro gusto.
     }
 
-    //Funcion para pedir los datos Madre o los datos Principales
+    std::string getNombre(){
+        return this -> nombre;
+    }
+
+    std::string getCurp(){
+        return this -> curp;
+    }
+
+    std::string getNacimiento(){
+        return this -> nacimiento;
+    }
+
+    int getEdad(){
+        return this -> edad;
+    }
+
+    int getYear(){
+        return this -> year;
+    }
+
+    int getPrestamo(){
+        return this -> prestamo;
+    }
+
+    int getDeducible(){
+        return this -> deducible;
+    }
+
+    virtual //Funcion para pedir los datos Madre o los datos Principales
     void SetDatos(){
         std::cout << "Ingresa Nombre: " << std::endl;
         std::cin >> nombre;
@@ -50,18 +74,33 @@ public:
         std::cout << "Ingresa Deducible: " << std::endl;
         std::cin >> deducible;
     }
+
+private:
+    std::string nombre;
+    std::string curp;
+    std::string nacimiento;
+    int edad, year, prestamo, deducible;
 };
 
-    //Poliza de Casa
+
+//Poliza de Casa
 class Casa : public Poliza{
 public:
-    float medida;
-    int antiguedad, postal, Nexterior;
-    std::string pais, estado, colonia, calle;
+
+    Casa (std::string nombre, std::string curp, std::string nacimiento, int year, int edad, int prestamo, int deducible, float medida, int antiguedad, int postal, int Nexterior, std::string pais, std::string estado, std::string colonia, std::string calle)
+    : Poliza (nombre, curp, nacimiento, year, edad, prestamo, deducible){
+        this -> medida = medida;
+        this -> antiguedad = antiguedad;
+        this -> postal = postal;
+        this -> Nexterior = Nexterior;
+        this -> pais = pais;
+        this -> estado = estado;
+        this -> colonia = colonia;
+        this -> calle = calle;
+    }
 
     //Funcion de Datos de Poliza de Casa
     void SetDatos() {
-        Poliza::SetDatos();
 
         std::cout << "Accediste a la poliza de Casa, ingresa los datos necesarios" << std::endl;
         std::cout << "Ingresa la medida de la casa en m2: " << std::endl;
@@ -69,7 +108,7 @@ public:
         std::cout << "Ingresa el anio en que se construyo: " << std::endl;
         std::cin >> antiguedad;
 
-            //Con este IF nos aseguramos de que el año de construccion que escriban es antes o sea menor al año actual
+        //Con este IF nos aseguramos de que el año de construccion que escriban es antes o sea menor al año actual
         if (antiguedad > 2023){
             std::cout << "El anio de construccion debe ser menor al año actual" << std::endl;
             exit(1);
@@ -89,27 +128,39 @@ public:
             std::cin >> Nexterior;
 
             //Aqui imprimimos todos los datos que se escribieron anteriormente, agregando los de la poliza señalada
-            std::cout << "El cliente " << nombre << " con " << edad << " anios de edad, de CURP " << curp
-            << " se ha registrado para una poliza de Casa con los siguientes datos:" << std::endl;
+            std::cout << "El cliente " << getNombre() << " con " << getEdad() << " anios de edad, de CURP " << getCurp()
+                      << "con un prestamo de $" << getPrestamo() << "y su deducible de $" << getDeducible()
+                      << " se ha registrado para una poliza de Casa con los siguientes datos:" << std::endl;
             std::cout << "Medida de casa en m2: " << medida << std::endl;
             std::cout << "Anio en que se construyo la casa: " << antiguedad << std::endl;
             std::cout << "Direccion de la Casa: " << pais << ", " << estado << ", " << postal << ", " << colonia
-            << ", "<< calle << ", " << Nexterior << std::endl;
+                      << ", "<< calle << ", " << Nexterior << std::endl;
             std::cout << "Folio de Poliza: " << Folio(*this) << std::endl;
         }
     }
+
+private:
+    float medida;
+    int antiguedad, postal, Nexterior;
+    std::string pais, estado, colonia, calle;
+
 };
 
 
-    //Poliza de Carro
+//Poliza de Carro
 class Carro : public Poliza{
 public:
-    int km, year, serial;
-    std::string marca, modelo;
+    Carro (std::string nombre, std::string curp, std::string nacimiento, int year, int edad, int prestamo, int deducible, std::string marca, std::string modelo, int km, int anio, int serial)
+    : Poliza (nombre, curp, nacimiento, year, edad, prestamo, deducible){
+        this -> marca = marca;
+        this -> modelo = modelo;
+        this -> km = km;
+        this -> anio = anio;
+        this -> serial = serial;
+    }
 
     //Funcion de Datos de Poliza de Carro
     void SetDatos(){
-        Poliza::SetDatos();
 
         std::cout << "Accediste a la poliza de Carro, ingresa los datos necesarios" << std::endl;
         std::cout << "Marca de carro:" << std::endl;
@@ -119,28 +170,36 @@ public:
         std::cout << "Kilometraje del carro: " << std::endl;
         std::cin >> km;
         std::cout << "Anio del carro: " << std::endl;
-        std::cin >> year;
+        std::cin >> anio;
         std::cout << "Numero de serie del carro: " << std::endl;
         std::cin >> serial;
 
         //Aqui imprimimos todos los datos que se escribieron anteriormente, agregando los de la poliza señalada
-        std::cout << "El cliente " << nombre << " con " << edad << " anios de edad, de CURP " << curp
-                  << " se ha registrado para una poliza de Carro con los siguientes datos:" << std::endl;
+        std::cout << "El cliente " << getNombre() << " con " << getEdad() << " anios de edad, de CURP " << getCurp()
+                  << "con un prestamo de $" << getPrestamo() << "y su deducible de $" << getDeducible()
+                  << " se ha registrado para una poliza de Casa con los siguientes datos:" << std::endl;
         std::cout << "Marca: " << marca << std::endl;
         std::cout << "Modelo: " << modelo << std::endl;
         std::cout << "Kilometraje: " << km << std::endl;
-        std::cout << "Anio: " << year << std::endl;
+        std::cout << "Anio: " << anio << std::endl;
         std::cout << "Numero de Serie: " << serial << std::endl;
         std::cout << "Folio de Poliza: " << Folio(*this) << std::endl;
     }
 
+private:
+    int km, anio, serial;
+    std::string marca, modelo;
 };
 
 class Vida : public Poliza {
 public:
-    std::string sangre;
-    std::string *enfermedades;
-    int num;
+    Vida (std::string nombre, std::string curp, std::string nacimiento, int year, int edad, int prestamo, int deducible, std::string sangre, std::string* enfermedades, int num)
+    : Poliza (nombre, curp, nacimiento, year, edad, prestamo, deducible){
+        this -> sangre = sangre;
+        this -> enfermedades = enfermedades;
+        this -> num = num;
+
+    }
 
     Vida() : enfermedades(nullptr), num(0) {}
 
@@ -180,25 +239,27 @@ public:
     }
 
     void SetDatos(){
-        Poliza::SetDatos();
 
         std::cout << "Accediste a la poliza de Vida, ingresa los datos necesarios" << std::endl;
         std::cout << "Tipo de Sangre: " << std::endl;
         std::cin >> sangre;
         setEnfermedades();
 
-        std::cout << "El cliente " << nombre << " con " << edad << " anios de edad, de CURP " << curp
-                  << " se ha registrado para una poliza de Vida con los siguientes datos:" << std::endl;
+        std::cout << "El cliente " << getNombre() << " con " << getEdad() << " anios de edad, de CURP " << getCurp()
+                  << "con un prestamo de $" << getPrestamo() << "y su deducible de $" << getDeducible()
+                  << " se ha registrado para una poliza de Casa con los siguientes datos:" << std::endl;
         std::cout << "Tipo de Sangre del cliente: " << sangre << std::endl;
         DatosEnfermedad();
         std::cout << "Folio de Poliza: " << Folio(*this) << std::endl;
     }
+
+private:
+    std::string sangre;
+    std::string* enfermedades;
+    int num;
 };
 
 void Principal(){
-    Casa casa;
-    Carro carro;
-    Vida vida;
 
     int elegir;
 
@@ -212,17 +273,20 @@ void Principal(){
     switch(elegir) {
 
         case 1: {
-            casa.SetDatos();
+            Casa casa();
+            casa().SetDatos();
             break;
         }
 
         case 2: {
-            carro.SetDatos();
+            Carro carro();
+            carro().SetDatos();
             break;
         }
 
         case 3: {
-            vida.SetDatos();
+            Vida vida();
+            vida().SetDatos();
             break;
         }
 
